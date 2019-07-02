19fb9ccb72f51f7647ac33652bcfc5f63bf53ebd
Matthew Herod <matthew.herod@myunidays.com>
2019-07-02 20:34:42 +0100

    Use nonstrict json

--- web/src/main/kotlin/dev/herod/bot/web/WebModule.kt
+++ web/src/main/kotlin/dev/herod/bot/web/WebModule.kt
@@ -6,12 +6,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.JsonSerializer
 import io.ktor.client.features.json.serializer.KotlinxSerializer
+import kotlinx.serialization.json.Json
 
 @Module
 class WebModule {
 
     @Provides
-    fun provideJsonSerializer(): JsonSerializer = KotlinxSerializer()
+    fun provideJsonSerializer(): JsonSerializer = KotlinxSerializer(json = Json.nonstrict)
 
     @Provides
     fun provideHttpClient(jsonSerializer: JsonSerializer): HttpClient = HttpClient {
