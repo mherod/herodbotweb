fba2775aaa16cef116c98435b1156e9a78be14fd
Matthew Herod <matthew.herod@myunidays.com>
2019-07-02 21:29:03 +0100

    getEnv -> System.getenv

--- web/src/main/kotlin/dev/herod/bot/foursquare/FoursquareClient.kt
+++ web/src/main/kotlin/dev/herod/bot/foursquare/FoursquareClient.kt
@@ -1,24 +1,24 @@
 package dev.herod.bot.foursquare
 
-import ch.qos.logback.core.util.OptionHelper
 import dev.herod.foursquare.FoursquareVenueResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.url
+import java.lang.System.getenv
 import javax.inject.Inject
 
 class FoursquareClient @Inject constructor(private val httpClient: HttpClient) {
 
     suspend fun venueSearch(longitude: String?, latitude: String?, intent: String = "checkin", query: String): FoursquareVenueResponse {
         return httpClient.get {
-            url("https://api.foursquare.com/v2/venues/search?intent=$intent&query=$query&ll=$longitude,$latitude&client_id=${OptionHelper.getEnv("FOURSQUARE_CLIENT_ID")}&client_secret=${OptionHelper.getEnv("FOURSQUARE_CLIENT_SECRET")}&v=20180323")
+            url("https://api.foursquare.com/v2/venues/search?intent=$intent&query=$query&ll=$longitude,$latitude&client_id=${getenv("FOURSQUARE_CLIENT_ID")}&client_secret=${getenv("FOURSQUARE_CLIENT_SECRET")}&v=20180323")
         }
     }
 
     suspend fun venueCheckin(venueId: String): String {
         return httpClient.post {
-            url("https://api.foursquare.com/v2/checkins/add?venueId=$venueId&client_id=${OptionHelper.getEnv("FOURSQUARE_CLIENT_ID")}&client_secret=${OptionHelper.getEnv("FOURSQUARE_CLIENT_SECRET")}&v=20180323")
+            url("https://api.foursquare.com/v2/checkins/add?venueId=$venueId&client_id=${getenv("FOURSQUARE_CLIENT_ID")}&client_secret=${getenv("FOURSQUARE_CLIENT_SECRET")}&v=20180323")
         }
     }
 }
