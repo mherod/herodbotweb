1cbc83fe42970a359042e03d3859438d0337e155
Matthew Herod <matthew.herod@myunidays.com>
2019-07-16 21:20:35 +0100

    Store all post data

--- web/src/main/kotlin/dev/herod/bot/web/routes/Routes.kt
+++ web/src/main/kotlin/dev/herod/bot/web/routes/Routes.kt
@@ -1,8 +1,14 @@
 package dev.herod.bot.web.routes
 
+import dev.herod.bot.db.DbConnection
 import dev.herod.bot.web.framework.RoutesInstaller
 import dev.herod.bot.web.framework.routes.FoursquareRoutesInstaller
+import dev.herod.bot.web.postBody
+import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
+import io.ktor.http.HttpMethod
+import io.ktor.request.httpMethod
+import io.ktor.request.uri
 import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
 import io.ktor.routing.Route
@@ -17,6 +23,21 @@ class Routes @Inject constructor(
 
     override fun install(route: Route) {
 
+        route.apply {
+            intercept(ApplicationCallPipeline.Setup) {
+                val request = call.request
+                if (request.httpMethod == HttpMethod.Post) {
+                    DbConnection.getMyDbConnection().use { connection ->
+                        connection.prepareStatement(
+                            "INSERT INTO \"postings\" (\"id\", \"blob\", \"url\") VALUES (DEFAULT, ?, ?)", arrayOf(
+                                request.uri,
+                                request.postBody()
+                            )
+                        )
+                    }
+                }
+            }
+        }
         route.route("/") {
             get {
                 call.respondText {
