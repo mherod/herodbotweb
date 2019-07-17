7fa74ed95cdd021d9920658ceefa6e48283d310a
Matthew Herod <matthew.herod@myunidays.com>
2019-07-17 18:35:39 +0100

    Add another samplewebhook test

--- models/src/test/kotlin/dev/herod/monzo/MonzoTransactionWebhookTest.kt
+++ models/src/test/kotlin/dev/herod/monzo/MonzoTransactionWebhookTest.kt
@@ -1,6 +1,7 @@
 package dev.herod.monzo
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.parse
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -10,7 +11,7 @@ class MonzoTransactionWebhookTest {
     @Test
     fun parseSampleWebhook() {
         val webhookJson = javaClass.getResource("samplewebhook.json").readText()
-        val webhook = Json.parse<MonzoTransactionWebhook>(webhookJson)
+        val webhook = Json(JsonConfiguration(strictMode = false)).parse<MonzoTransactionWebhook>(webhookJson)
         val type = webhook.type
         assertEquals(type, "transaction.created")
         val data = webhook.data
@@ -18,5 +19,18 @@ class MonzoTransactionWebhookTest {
         val address = data.merchant.address
         assertEquals(address.city, "London")
         assertEquals(address.country, "GB")
+    }
+
+    @Test
+    fun parseSampleWebhook2() {
+        val webhookJson = javaClass.getResource("samplewebhook2.json").readText()
+        val webhook = Json(JsonConfiguration(strictMode = false)).parse<MonzoTransactionWebhook>(webhookJson)
+        val type = webhook.type
+        assertEquals(type, "transaction.created")
+        val data = webhook.data
+        assertEquals(data.description, "AMAZON UK RETAIL       WWW.AMAZON.CO LUX")
+        val address = data.merchant.address
+        assertEquals(address.city, "")
+        assertEquals(address.country, "LUX")
     }
 }
