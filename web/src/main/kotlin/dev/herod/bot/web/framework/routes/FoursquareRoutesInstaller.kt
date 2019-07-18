6f1ca412d8b5f1701926ef273ae8057e54ec7adb
Matthew Herod <matthew.herod@myunidays.com>
2019-07-18 22:54:51 +0100

    Add search endpoint

--- web/src/main/kotlin/dev/herod/bot/web/framework/routes/FoursquareRoutesInstaller.kt
+++ web/src/main/kotlin/dev/herod/bot/web/framework/routes/FoursquareRoutesInstaller.kt
@@ -2,18 +2,30 @@ package dev.herod.bot.web.framework.routes
 
 import dev.herod.bot.foursquare.FoursquareClient
 import dev.herod.bot.web.framework.RoutesInstaller
+import dev.herod.bot.web.respondJson
 import dev.herod.monzo.MonzoTransactionWebhook
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import io.ktor.routing.get
 import io.ktor.routing.post
 import javax.inject.Inject
 
 class FoursquareRoutesInstaller @Inject constructor(private val foursquareClient: FoursquareClient) : RoutesInstaller {
 
     override fun install(route: Route) {
+
+        route.get("/search") {
+            call.respondJson {
+                foursquareClient.searchVenue(
+                    longitude = "${parameters["lon"]}",
+                    latitude = "${parameters["lat"]}",
+                    query = "${parameters["q"]}"
+                ).response?.venues.orEmpty()
+            }
+        }
 
         route.post("/monzo") {
 
