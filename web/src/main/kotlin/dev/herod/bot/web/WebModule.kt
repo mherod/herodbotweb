6bbcf06cb4c3769f774b908f219fa54a9b4aafea
Matthew Herod <matthew.herod@myunidays.com>
2019-07-19 17:16:41 +0100

    Define apache http engine explictly

--- web/src/main/kotlin/dev/herod/bot/web/WebModule.kt
+++ web/src/main/kotlin/dev/herod/bot/web/WebModule.kt
@@ -3,6 +3,7 @@ package dev.herod.bot.web
 import dagger.Module
 import dagger.Provides
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.JsonSerializer
 import io.ktor.client.features.json.serializer.KotlinxSerializer
@@ -15,7 +16,7 @@ class WebModule {
     fun provideJsonSerializer(): JsonSerializer = KotlinxSerializer(json = Json.nonstrict)
 
     @Provides
-    fun provideHttpClient(jsonSerializer: JsonSerializer): HttpClient = HttpClient {
+    fun provideHttpClient(jsonSerializer: JsonSerializer): HttpClient = HttpClient(Apache) {
         install(JsonFeature) {
             serializer = jsonSerializer
         }
