35ddafbef509b9a34d25d498326a0d62ea9bcefc
Matthew Herod <matthew.herod@myunidays.com>
2019-07-19 18:07:50 +0100

    Try fix venue checkin

--- web/src/main/kotlin/dev/herod/bot/foursquare/FoursquareClient.kt
+++ web/src/main/kotlin/dev/herod/bot/foursquare/FoursquareClient.kt
@@ -6,23 +6,48 @@ import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.url
 import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
 import io.ktor.http.contentType
 import java.lang.System.getenv
 import javax.inject.Inject
 
 class FoursquareClient @Inject constructor(private val httpClient: HttpClient) {
 
-    suspend fun searchVenue(longitude: String?, latitude: String?, intent: String = "checkin", query: String): FoursquareVenueResponse {
+    suspend fun searchVenue(
+        longitude: String?,
+        latitude: String?,
+        intent: String = "checkin",
+        query: String
+    ): FoursquareVenueResponse {
         return httpClient.get {
-            url("https://api.foursquare.com/v2/venues/search?intent=$intent&query=$query&ll=$longitude,$latitude&client_id=${getenv("FOURSQUARE_CLIENT_ID")}&client_secret=${getenv("FOURSQUARE_CLIENT_SECRET")}&v=20180323")
+            url(
+                "https://api.foursquare.com/v2/venues/search?intent=$intent&query=$query&ll=$longitude,$latitude&client_id=${getenv(
+                    "FOURSQUARE_CLIENT_ID"
+                )}&client_secret=${getenv("FOURSQUARE_CLIENT_SECRET")}&v=20180323"
+            )
         }
     }
 
     suspend fun venueCheckin(venueId: String): String {
+
+        val params = mapOf(
+            "venueId" to venueId,
+            "client_id" to getenv("FOURSQUARE_CLIENT_ID"),
+            "client_secret" to getenv("FOURSQUARE_CLIENT_SECRET"),
+            "v" to "20180323"
+        )
+
         return httpClient.post {
-            url("https://api.foursquare.com/v2/checkins/add?venueId=$venueId&client_id=${getenv("FOURSQUARE_CLIENT_ID")}&client_secret=${getenv("FOURSQUARE_CLIENT_SECRET")}&v=20180323")
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "api.foursquare.com"
+                path("v2", "checkins", "add")
+                for ((key, value) in params) {
+                    parameters.append(key, value)
+                }
+            }
             contentType(ContentType.Application.Json)
-            body = mapOf("venueId" to venueId)
+            body = params
         }
     }
 }
