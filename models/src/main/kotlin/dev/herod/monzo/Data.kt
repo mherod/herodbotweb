c96492b5e5ecb852a4b6a2e6ccd32d0bc8379d1f
Matthew Herod <matthew.herod@myunidays.com>
2019-07-20 14:22:28 +0100

    Fix Gson deserialization of webhook

--- models/src/main/kotlin/dev/herod/monzo/Data.kt
+++ models/src/main/kotlin/dev/herod/monzo/Data.kt
@@ -1,39 +1,31 @@
 package dev.herod.monzo
 
-import kotlinx.serialization.Optional
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Data(
-    @Optional
+    @SerializedName("account_id")
     @SerialName("account_id")
     val accountId: String = "",
-    @Optional
     @SerialName("amount")
     val amount: Int = 0,
-    @Optional
     @SerialName("category")
     val category: String = "",
-    @Optional
     @SerialName("created")
     val created: String = "",
-    @Optional
     @SerialName("currency")
     val currency: String = "",
-    @Optional
     @SerialName("description")
     val description: String = "",
-    @Optional
     @SerialName("id")
     val id: String = "",
-    @Optional
+    @SerializedName("is_load")
     @SerialName("is_load")
     val isLoad: Boolean = false,
-    @Optional
     @SerialName("merchant")
     val merchant: Merchant = Merchant(),
-    @Optional
     @SerialName("settled")
     val settled: String = ""
 )
