65cfdfebc4f893b4311ce955b2781aa817187579
Matthew Herod <matthew.herod@myunidays.com>
2019-07-24 18:54:56 +0100

    Update Util.kt

--- core/src/main/kotlin/dev/herod/bot/Util.kt
+++ core/src/main/kotlin/dev/herod/bot/Util.kt
@@ -4,13 +4,13 @@ import java.io.File
 
 fun getEnv(name: String): String {
     return runCatching { getEnv(name) }
-            .getOrElse { getEnvFromFile(name) }
+        .getOrElse { getEnvFromFile(name) }
 }
 
 private fun getEnvFromFile(name: String): String {
     return runCatching {
         File("../.env").readLines()
-                .first { it.startsWith("$name=") }
-                .substringAfter('=')
+            .first { it.startsWith("$name=") }
+            .substringAfter('=')
     }.getOrThrow()
 }
